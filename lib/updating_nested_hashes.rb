dd10a53bcbf2d3d47becb4b360f846a2fc076b14
Meredith McIntosh <meredith@mcintosh.io>
2020-09-17 22:47:01 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -32,6 +32,7 @@ def update_status
   # After your code, the following code will return the altered hash. This method must only return the modified epic_tragedy hash
   epic_tragedy
 end
+update_status
 
 def add_characters
    epic_tragedy = {
